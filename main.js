8aa9d46f924d4bb68511eba0aece120f1ebc3c68
AriesJ <ajtagle12@gmail.com>
2025-07-09 14:27:08 +0800

    creating the scene

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-import * as THREE from 'three';
+import * as THREE from 'three'; // imports library
+
+
+// creates the scene(this is the one responsible for rendering)
+const scene = new THREE.Scene();
+
+//adding object(camera)
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+//renders(more like play) the scene
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );

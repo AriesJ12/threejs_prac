3b0fb35f78592f67fbe050d9d9c9bd2f5d0c9531
AriesJ <ajtagle12@gmail.com>
2025-07-09 14:31:16 +0800

    added comments for better understanding on camera

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ const scene = new THREE.Scene();
 
 //adding object(camera)
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// parameters:
+//  Field of view - how wide can the camera see
+// aspect ratio
+// near value - responsible for not showing objects that are too near (n < near = wont show)
+// far value - same as near but for far (n > far = wont show)
 
 //renders(more like play) the scene
 const renderer = new THREE.WebGLRenderer();

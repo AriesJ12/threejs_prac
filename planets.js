648c64d3b53257c062609065d79ceb02b34efa9d
AriesJ <ajtagle12@gmail.com>
2025-07-21 14:00:19 +0800

    add

--- planets.js
+++ planets.js
@@ -21,3 +21,11 @@ document.body.appendChild(renderer.domElement);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// Lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);

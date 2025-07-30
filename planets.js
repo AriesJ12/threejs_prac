0f4cb66d8475f3737687e638956e05ccafd021c3
AriesJ <ajtagle12@gmail.com>
2025-07-30 15:45:58 +0800

    change earth size

--- planets.js
+++ planets.js
@@ -70,10 +70,10 @@ scene.add(glowMesh);
 
 const earthTexture = textureLoader.load("./planets/2k_earth_normal_map.tif")
 
-const eartGeometry = new THREE.SphereGeometry(1.5, 64, 64)
+const eartGeometry = new THREE.SphereGeometry(1.3, 64, 64)
 const earthMaterial = new THREE.MeshStandardMaterial({map: earthTexture})
 const earth = new THREE.Mesh(eartGeometry, earthMaterial)
-earth.position.x = 5
+earth.position.x = 6
 scene.add(earth)
 
 // Animation
